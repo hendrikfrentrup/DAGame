c6af49c52348ede2fd945ad1569c5094eceedb6c
Hendrik Frentrup <hendrik.frentrup@gmail.com>
2018-08-17 11:50:05 +0100

    fix typo

--- index.js
+++ index.js
@@ -139,6 +139,6 @@ io.on('connection', function(socket){
 
 var process = require('process');
 
-http.listen(process.env.PORT || 5000, function(){
+http.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:3000');
 });
