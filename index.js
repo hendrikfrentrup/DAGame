72a57cd8b5237d8e13dbce9839f28d026daf8695
Hendrik Frentrup <hendrik.frentrup@gmail.com>
2018-08-17 11:49:00 +0100

    fix port 80 issue and bind to port

--- index.js
+++ index.js
@@ -137,6 +137,8 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(80, function(){
-  console.log('listening on *:80');
+var process = require('process');
+
+http.listen(process.env.PORT || 5000, function(){
+  console.log('listening on *:3000');
 });
